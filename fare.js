c9caac4e82d2e4f86dcc02c3c4f119be9d5257c4
ivy <ivy.chemutai@student.moringaschool.com>
2025-06-08 00:27:36 +0300

    Boda Boda Fare Estimator

---
+++ fare.js
@@ -0,0 +1,14 @@
+function calculateBodaFare (){
+    const input=prompt('Unafika wapi Mkubwa? Kilometer ngapi?:');
+    const distance =Number(5)
+}
+const basefare=(50);
+const PerKilometerCharge=(15);
+const totalFare = basefare + ('distance * PerKilometerCharge');
+ 
+console.log('Base Fare:KES {50}');
+console.log('PerKilometerCharge:KES {15}');
+console.log('Trip Distance:{5}');
+console.log('Total Fare:{75}');
+console.log('Panda Pikipiki');
+
