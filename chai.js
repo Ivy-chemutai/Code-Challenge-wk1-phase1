e8b6ccf6f4256ab390033b885136aa6a7f2bfd86
ivy <ivy.chemutai@student.moringaschool.com>
2025-06-07 22:44:09 +0300

    initial commit:Chai Borá Ingredient Calculator

---
+++ chai.js
@@ -0,0 +1,22 @@
+function calculateChaiIngredients(){
+    const input= prompt('Karibu! How many cups of Chai Bora would you like tomake?');
+    const numberOfCups= Number(input);
+    }
+
+const waterPerCup =600;
+const milkPerCup =150;
+const teaLeavesPerCup = 3;
+const sugarPerCup =6;
+
+const totalWater =waterPerCup*3;
+const totalMilk =milkPerCup*3;
+const  totalTeaLeaves =teaLeavesPerCup*3;
+const  totalSugar = sugarPerCup*3;
+
+console.log('To make {3}cups of Kenyan Chai, you will need:');
+console.log('Water:{600} ml');
+console.log('Milk:{150} ml');
+console.log('Tea Leaves (Majani):{3} tablespoon');
+console.log('sugar (Sukari):{6} teaspoon');
+console.log('Enjoy your Chai Bora!');
+
