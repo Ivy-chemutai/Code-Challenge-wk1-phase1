d3e8a363e339a11d760be54025da5ad146a28078
chemutai .ivy <ivy.chemutai@student.moringaschool.com>
2025-06-10 12:04:58 +0300

    Update fee.js

--- fee.js
+++ fee.js
@@ -1,3 +1,4 @@
+// Function to estimate transaction fee based on the amount sent
 function estimateTransactionFee() {
   let input = prompt("Unatuma Ngapi?")
   let amountToSend = Number(input);
@@ -6,13 +7,16 @@ function estimateTransactionFee() {
   let totalAmount = amountToSend + fee;
   
 }
+// Check if the input is a valid number
 
   console.log('Sending KES 500');
 console.log('Calculated Transaction Fee: KES 10');
 console.log ('Total amount to be debited: KES 510')
 console.log('Send Money Securely!');
 
+// // Display the results
 console.log('Sending KES 2000');
 console.log('Calculated Transaction Fee: KES 30');
 console.log('Total amount to be debited:KES 2030');
+
 
